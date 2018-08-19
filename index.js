68b7a04da584df8c2e8893f0e908f8f52a665493
alex-avila <alex.avilx@icloud.com>
2018-08-19 17:43:57 -0600

    Testing pre-commit hook

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const app = express();
-require("dotenv").config();
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
-const port = process.env.PORT || 8000;
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const port = process.env.PORT || 8000
 
-app.use(morgan("dev"));
-app.use(bodyParser.json());
+app.use(morgan('dev'))
+app.use(bodyParser.json())
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(port, () => console.log(`Server is running on port ${port}`))
